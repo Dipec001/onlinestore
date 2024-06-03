007d5e7618c659b30ea2c4c92472f235d6328194
Dipec001 <dpecchukwu@gmail.com>
2024-06-03 21:09:24 +0100

    "Updated ALLOWED_HOSTS in settings.py to include heroku app domain"

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['epharma-91ebb7c041f9.herokuapp.com']
 
 
 # Application definition
