20c08fb75201a5969ecf650c1e8bbc1fed6c1bf8
Dipec001 <dpecchukwu@gmail.com>
2024-06-04 11:25:05 +0100

    "Commented out default database settings in settings.py and updated corresponding pycache file."

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -83,15 +83,15 @@ TEMPLATES = [
 WSGI_APPLICATION = "onlinestore.wsgi.application"
 
 
-# Database
+# Database\
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+# # DATABASES = {
+# #     "default": {
+# #         "ENGINE": "django.db.backends.sqlite3",
+# #         "NAME": BASE_DIR / "db.sqlite3",
+# #     }
+# # }
 
 # if DEBUG:
 #     DATABASES = {
