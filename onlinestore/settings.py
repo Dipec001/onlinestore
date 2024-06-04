d8da4e40476a6bfe17c0f7b71d80135b3b3168a9
Dipec001 <dpecchukwu@gmail.com>
2024-06-04 11:44:04 +0100

    "Updated settings.py, styles.css, and index.html files"

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -86,35 +86,29 @@ WSGI_APPLICATION = "onlinestore.wsgi.application"
 # Database\
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# # DATABASES = {
-# #     "default": {
-# #         "ENGINE": "django.db.backends.sqlite3",
-# #         "NAME": BASE_DIR / "db.sqlite3",
-# #     }
-# # }
 
-# if DEBUG:
-#     DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("DB_ENGINE"),
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT"),
-#         "CONN_MAX_AGE": 300,  # Maximum connection age in seconds (e.g., 5 minutes)
-#         'CONN_MAX_NUM': 20,   # Maximum number of connections in the pool
-#     }
-# } 
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
-#     }
-
-# Database configuration
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "CONN_MAX_AGE": 300,  # Maximum connection age in seconds (e.g., 5 minutes)
+        'CONN_MAX_NUM': 20,   # Maximum number of connections in the pool
+    }
+} 
+else:
+    DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+# Database configuration
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+# }
 
 
 # Password validation
