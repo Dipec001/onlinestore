f6aea8ad3e150b4ce0e3cb5196828e5d746107ac
Dipec001 <dpecchukwu@gmail.com>
2024-06-04 10:18:09 +0100

    "Updated settings.py and styles.css files"

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -93,28 +93,28 @@ WSGI_APPLICATION = "onlinestore.wsgi.application"
 #     }
 # }
 
-if DEBUG:
-    DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "CONN_MAX_AGE": 300,  # Maximum connection age in seconds (e.g., 5 minutes)
-        'CONN_MAX_NUM': 20,   # Maximum number of connections in the pool
-    }
-} 
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
-    }
+# if DEBUG:
+#     DATABASES = {
+#     "default": {
+#         "ENGINE": os.getenv("DB_ENGINE"),
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#         "CONN_MAX_AGE": 300,  # Maximum connection age in seconds (e.g., 5 minutes)
+#         'CONN_MAX_NUM': 20,   # Maximum number of connections in the pool
+#     }
+# } 
+# else:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+#     }
 
-# #     # Database configuration
-# DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-# }
+#     # Database configuration
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
